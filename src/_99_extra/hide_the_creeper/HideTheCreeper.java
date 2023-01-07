891c2c45ccfb19c1751fc6638a2ec94bc16bcb2e
league <league@wts14-3.attlocal.net>
2023-01-07 12:56:41 -0800

    done

--- src/_99_extra/hide_the_creeper/HideTheCreeper.java
+++ src/_99_extra/hide_the_creeper/HideTheCreeper.java
@@ -1,27 +1,48 @@
 package _99_extra.hide_the_creeper;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class HideTheCreeper extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
+	static final int WIDTH = 600;
+	static final int HEIGHT = 400;
+	PImage creeper;
 
-    @Override
-    public void settings() {
-        size(WIDTH, HEIGHT);
-    }
+	@Override
+	public void settings() {
+		size(WIDTH, HEIGHT);
+	}
 
-    @Override
-    public void setup() {
-        
-    }
+	@Override
+	public void setup() {
+		PImage minecraft = loadImage("grass.jpeg");
+		minecraft.resize(WIDTH, HEIGHT);
+		background(minecraft);
+		creeper = loadImage("creeper.jpeg");
+		creeper.resize(5,5);
+	}
 
-    @Override
-    public void draw() {
-        
-    }
+	@Override
+	public void draw() {
+		image (creeper, 200, 300);
 
-    static public void main(String[] args) {
-        PApplet.main(HideTheCreeper.class.getName());
-    }
+	}
+	public void mousePressed() {
+		fill(265,0,0);
+		ellipse(mouseX, mouseY, 10, 10); 		
+		if(isNear(mouseX,200)&& isNear(mouseY, 300)) {
+			fill(0,265,0);
+			ellipse(mouseX, mouseY, 10, 10);
+		}
+	}
+	boolean isNear(int a, int b) {
+		if (abs(a - b) < 10)
+		     return true;
+		else
+		     return false;
+		}
+	
+	static public void main(String[] args) {
+		PApplet.main(HideTheCreeper.class.getName());
+	}
 }
